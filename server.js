d789d822c58937504314ce7e6df93185da9ebf42
obuchnea <obuchnea1@gmail.com>
2026-06-03 12:24:33 -0400

    updated server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: '*'}));
 
 // ─── HubSpot helpers ─────────────────────────────────────────────────────────
 
